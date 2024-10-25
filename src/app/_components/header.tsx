7dc637d66cd541a8e57688f579b4d4d8c6855ad1
Feijaoozin <gonardo1234@gmail.com>
2024-10-25 01:01:43 -0300

    feat: add header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,43 @@
+import { Menu } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger, 
+  SheetClose } from "./ui/sheet";
+import Link from "next/link";
+
+
 export default function Header() {
  return (
-   <div>
+   <div className="bg-pink-950 flex justify-between p-5">
     <h1>Jana.com</h1>
+    <Sheet>
+
+      <SheetTrigger>
+        <Badge>
+          <Menu size={24}/>
+        </Badge>
+      </SheetTrigger>
+
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+
+      <SheetClose>
+        <Link href="/">
+          <Badge>
+              Início
+          </Badge>
+        </Link>
+      </SheetClose>
+
+      </SheetContent>
+
+    </Sheet>
+
    </div>
  );
 }
