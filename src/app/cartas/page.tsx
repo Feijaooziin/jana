1159abc445593ed758e5d6b22f64a83ed77a856c
Feijaoozin <gonardo1234@gmail.com>
2024-10-26 20:10:02 -0300

    feat: add page "cartas"

--- src/app/cartas/page.tsx
+++ src/app/cartas/page.tsx
@@ -4,10 +4,193 @@ export default function Cartas() {
  return (
     <Main>
         <div className="flex flex-col items-center h-full">
-            <h1 className="text-4xl p-6 underline underline-offset-4">CARTAS PARA JANAÍNA</h1>
+            <h1 className="text-4xl pt-6 mb-20 underline underline-offset-4">CARTAS PARA JANAÍNA</h1>
 
-            <div>
-                <span>Curitiba, 09 de Setembro de 2020</span>
+            {/* TEXTO 1 */}
+            <div className="flex flex-col p-4 border-b-2 border-dashed">
+                <span className="text-2xl text-center pb-5 font-bold italic">
+                    Curitiba, 09 de Setembro de 2020
+                </span>
+                <p>
+                    Meu bem, me perdoe se não estou ao seu lado agora. Sei que seria ainda melhor ouvir essas palavras de amor se nossas mãos estivessem entrelaçadas. Mas a distância e as circunstâncias me impedem, então o que posso é declarar o meu amor através desse texto.
+                    <br/><br/>
+                    Então, enquanto estiver lendo ele, imagine nossos lábios se tocando, os mais belos gestos para demonstrar o meu carinho. Ainda não posso morar aí pertinho de você, mas esse dia vai chegar, eu tenho certeza. O que mais sonho todos os dias é poder te abraçar, falar ao seu ouvido, te ter por perto. Meu sentimento, porém, segue firme e forte como sempre.
+                    <br/><br/>
+                    Sabia que meus pensamentos de todos os dias estão com você? Você não está aqui do meu lado, mas está sempre no meu coração. Quilômetros e quilômetros nos separam, eu sei, mas distância nenhuma é capaz de vencer o que eu sinto por você. Eu te espero, com um sorriso nos lábios e a mais pura alegria em minha mente. Por mais que demore muito tempo para nos vermos cara a cara novamente, o meu sentimento não vacila, nem por um segundo. Meu coração sempre pertencerá a você e isso me faz uma pessoa completa, e muito mais feliz!
+                    <br/><br/>
+                    Desde que conheci você, entendo como é valioso um abraço, uma mensagem, um áudio, até uma conversa pela internet. Qualquer contato vale, mesmo que digital, enquanto essa distância não permite que você esteja aqui ao meu lado, bem perto de mim. As palavras substituem os toques que eu tanto espero. Ainda posso ver seu lindo rosto, mesmo que por uma tela.
+                    <br/><br/>
+                    Crio as mais perfeitas lembranças cada vez que ouço a sua voz nos áudio que ainda tenho salvo. E, é claro, sempre tenho você em meus sonhos. Enquanto te espero, estou sozinho, mas não solitário. A casa fica vazia, mas minha mente está sempre repleta de você.
+                    <br/><br/>
+                    Com tantas tecnologias, que dia o homem vai inventar uma máquina de se teletransportar? A internet encurta as distâncias, mas nada substitui a sensação de tocar a sua pele, de beijar o seu rosto, de passar as mãos nos seus cabelos, de sentir o seu cheiro.
+                    <br/><br/>
+                    É verdade que ajuda muito poder ouvir a sua voz nos áudios antigos que você me mandou, poder ver o seu rosto nas fotos que você posta, mas isso só aumenta a vontade de ter você perto de mim, só aumenta o desejo de abraçar e beijar você.
+                    <br/><br/>
+                    Às vezes penso que no tempo das cartas, os namorados distantes até pareciam estar mais próximos. Porque ao menos na carta eles recebiam o papel que o ser amado tocou, e podiam sentir o perfume do outro.
+                    <br/><br/>
+                    Bom mesmo vai ser quando chegar o dia que não vamos depender de nada, de nenhum tipo de tecnologia, para nos vermos, para falarmos um com o outro e para nos abraçarmos. Desejo é que chegue logo o dia que estaremos pertinho um do outro, e que essa distância que nos separa acabe para sempre.
+                    <br/><br/>
+                    Eu te amo de longe, mas te tenho dentro do meu coração!
+                </p>
+            </div>
+
+            {/* TEXTO 2 */}
+            <div className="flex flex-col p-4 mt-12 border-b-2 border-dashed">
+                <span className="text-2xl text-center pb-5 font-bold italic">
+                    Curitiba, 10 de Setembro de 2020
+                </span>
+                <p>
+                    Oi, vim desejar um bom dia pra pessoa que não sai da minha cabeça! A pessoa que foi a razão do meu sorriso bobo durante dias.
+                    <br/><br/>
+                    Hoje na empresa, teve uma hora que eu cochilei e eu sonhei com você, aí eu acordei precisando de uma respiração boca a boca sua, porque me afoguei em tanta beleza.
+                    <br/><br/>
+                    Conhecer você foi a melhor coisa que me aconteceu nos últimos tempos. Das coisas da minha vida, certamente
+                    <span className="font-bold"> VOCÊ </span>
+                    foi melhor de todas!
+                    <br/><br/>
+                    Se o universo olhasse o quanto você é linda, ele teria vergonha em ser tão pequeno comparado com a sua beleza.
+                    <br/><br/>
+                    E se eu pudesse te dar algo na vida, eu te daria a habilidade de te ver com meus olhos, só assim entenderia o motivo de eu te achar tão incrível assim...
+                    <br/><br/>
+                    Obrigado por ser
+                    <span className="font-bold"> SENSACHOCRÍVEL </span>
+                    .
+                </p>
+            </div>
+
+            {/* TEXTO 3 */}
+            <div className="flex flex-col p-4 border-b-2 border-dashed">
+                <span className="text-2xl text-center pb-5 font-bold italic">
+                    Curitiba, 08 de Novembro de 2020
+                </span>
+                <p>
+                    Oi amor da minha vida, sei que fiquei todo esse tempo sem escrever pra você, mas é que eu estava fazendo uma coisa melhor, eu estava dedicando todo meu tempo a você pessoalmente. Desculpa pela falta de textos.
+                    <br/><br/>
+                    <span className="font-bold underline underline-offset-2">
+                    N.E.O.Q.E.A.V.
+                    </span>
+                    <br/><br/><br/>
+                    <span className="font-bold text-2xl">
+                    J-A-N-A
+                    </span>
+                    <br/><br/>
+
+                    Já te disse muitas vezes<br/>
+                    Acho que não foi suficiente<br/>
+                    Nos seus olhos tem estrelas<br/>
+                    Ainda mais lindas que o sol Nascente<br/>
+                    <br/><br/>
+                    Juro que não entendo<br/>
+                    As vezes que você chora<br/>
+                    Não conseguindo enxergar<br/>
+                    A beleza que vejo toda hora<br/>
+                    <br/><br/>
+                    Jogo palavras pra você<br/>
+                    As mais belas que consigo<br/>
+                    Nunca vão ser o suficiente<br/>
+                    Ainda são poucas as que eu digo<br/>
+                    <br/><br/>
+                    Já te falei mil vezes<br/>
+                    Acho que dez mil ou mais ainda<br/>
+                    Na verdade já perdi as contas<br/>
+                    Aaaaaaa, COMO VOCÊ É LINDA <br/>
+                </p>
+            </div>
+
+            {/* TEXTO 4 */}
+            <div className="flex flex-col p-4 border-b-2 border-dashed">
+                <span className="text-2xl text-center pb-5 font-bold italic">
+                    Colombo, 23 de Dezembro de 2020
+                </span>
+                <p>
+                    Meu amor, nunca imaginei que um dia você pudesse ficar ainda mais linda do que era, mas desde que nós recebemos a notícia do nosso presente que está vindo do céu para entrar em nossas vidas, uma luz linda passou a iluminar o seu rosto. Você está cada dia mais bonita, mais forte, mais apaixonante.
+                    <br/><br/>
+                    Quando olho para você e para a sua barriga, e penso no nosso bebê aí dentro, tão guardadinho, o amor toma conta de mim, do meu coração, do meu corpo e da minha alma. Nunca pensei que existia amor assim, tão intenso, tão pleno e tão feliz.
+                    <br/><br/>
+                    Você é a mulher da minha vida, me sinto muito realizado com tudo que está acontecendo conosco, e sei que está só começando, a nossa felicidade ainda vai crescer muito mais, vamos ficar muito mais unidos do que antes.
+                    <br/><br/>
+                    Me sinto realmente um privilegiado por ser o seu namorado, por ser o pai deste filho que você está guardando para nós.
+                    <br/><br/>
+                    Você é a mulher dos meus sonhos e quero estar ao seu lado para sempre. Seu e da nossa filha que está em seu ventre, esperando para nascer.
+                    <br/><br/>
+                    A cada dia que passa, tenho ainda mais certeza que encontrei a mulher da minha vida. Mal posso esperar para o nascimento do nosso bebê.
+                    <br/><br/>
+                    Nossa filha veio para aumentar nossa família e tornar você a mãe mais linda do mundo. Você nunca esteve tão linda e radiante. Minha esposa e, agora, mãe da minha filha.
+                    <br/><br/>
+                    A gravidez pode trazer dificuldade, mas, ainda assim, você segue tão linda que meu amor por você só aumenta.
+                    <br/><br/>
+                    Torço para que nossa filha nasça e seja tão maravilhosa e forte como você. Tenho certeza que será!
+                    <br/><br/>
+                    Obrigado pelo seu amor, obrigado por me fazer o homem mais feliz de todo mundo! Eu amo muito você, amo muito o que nós dois temos, e vou amar ainda mais nós três!
+                    <br/><br/>
+                </p>
+            </div>
+
+            {/* TEXTO 5 */}
+            <div className="flex flex-col p-4 border-b-2 border-dashed">
+                <span className="text-2xl text-center pb-5 font-bold italic">
+                    Colombo, 24 de Março de 2021
+                </span>
+                <p>
+                    Sabe, eu gostaria de ter o dom de expressar em palavras tudo o que eu sinto por você, mas infelizmente não sou um poeta. Às vezes acho que nem mesmo se eu fosse um poeta conseguiria explicar para você esse meu sentimento.
+                    <br/><br/>
+                    Pois como explicar com palavras algo que se sente tão profundo? Como explicar o que é esse coração acelerado, quando você chega? Ou esse desespero quando você sai? Essa alegria sem fim quando você me abraça? Ou esse calor imenso quando você me beija?
+                    <br/><br/>
+                    Você consegue desestruturar o meu ser de uma forma deliciosa, e eu chego à conclusão apenas de uma coisa: eu te amo com todas as forças do meu ser, de maneira egoísta, pois eu quero só pra mim; de maneira suave, porque me sinto flutuar quando estou ao seu lado, mas principalmente de maneira plena, pois você me completa.
+                    <br/><br/>
+                    Se hoje conheço o verdadeiro significado da palavra amor, é porque você me ensinou.
+                    <br/><br/>
+                    Que alegria comemorar essa data pela primeira vez com você. A cada dia desejo mais que possamos viver muitos e muitos anos juntos.
+                    <br/><br/>
+                    Agradeço a sua compreensão, o seu apoio e por estar sempre comigo nos momentos alegres e tristes da minha vida.
+                    <br/><br/>
+                    Agradeço esse convívio gostoso que você me proporciona.
+                    <br/><br/>
+                    Agradeço por você ser do jeito que é e por se tornar tão essencial em minha vida.
+                    <br/><br/>
+                    Você é linda, carinhosa, perfeita até na imperfeição. Você mudou meu mundo, trouxe significado e transformou minha vida em uma permanente alegria. Meu amor por você não conhece limites ou barreiras, e meu objetivo diário é fazer você feliz.
+                    <br/><br/>
+                    Seja feliz, é tudo que desejo, e lembre-se que se você for feliz, também eu sou.
+                    <br/><br/>
+                    Você é a mulher da minha vida; eu amo você e amo a vida ao seu lado.
+                    <br/><br/>
+                    A sua felicidade é a minha felicidade, por isso nunca deixarei de lutar para que você alcance todos os seus sonhos.
+                    <br/><br/>
+                    Parabéns pelo seu aniversário, e tenha um dia muito feliz! 
+                    <br/><br/>
+                </p>
+            </div>
+
+            {/* TEXTO 6 */}
+            <div className="flex flex-col p-4 border-b-2 border-dashed">
+                <span className="text-2xl text-center pb-5 font-bold italic">
+                    Colombo, 24 de Março de 2022
+                </span>
+                <p>
+                    Oi meu amor, faz exatamente um ano que não escrevo nada. me perdoa, mas você sabe a correria que tem sido pra nós. Enfim, eu amo você!
+                    <br/><br/>
+                    Em todos os seus aniversários, eu só penso em agradecer aos céus por você ter nascido.
+                    <br/><br/>
+                    Eu não seria nada sem você, por isso hoje, no dia do seu aniversário eu quero te lembrar tudo que você significa para mim.
+                    <br/><br/>
+                    Você é minha força, quando estou fraco e minha coragem quando me amedronto, minha sabedoria quando eu nem consigo pensar direito, é a calmaria no meio de todas as tempestades (e olha que ultimamente estou tendo muitas, algumas você nem sabe).
+                    <br/><br/>
+                    Mais uma vez estamos juntos para comemorar seu aniversário e assim como em todos os outros anos, vou fazer de tudo para que seja um dia lindo e repleto de amor para você.
+                    <br/><br/>
+                    Mesmo sem você se esforçar, me traz felicidades todos os dias, pois tudo que preciso para ter um sorriso no rosto é olhar para os seus olhos e ver que meu amor é correspondido e tenho a parceira que pedi para Deus.
+                    <br/><br/>
+                    A cada ano que passa você fica mais incrível e tenho mais certeza de que é com você que quero passar o resto da minha vida!
+                    <br/><br/>
+                    Nossa vida juntos é tudo que eu sempre sonhei. Ter você como esposa é a melhor coisa que já me aconteceu e quero que você saiba que eu dou muito valor ao nosso relacionamento e tudo que temos juntos.
+                    <br/><br/>
+                    Quero que tenha um aniversário fantástico e que todos os seus sonhos se realizem, pois nada me faria mais feliz do que a sua felicidade.
+                    <br/><br/>
+                    Espero que nós possamos compartilhar sorrisos e alegrias hoje e sempre.
+                    <br/><br/>
+                    <span className="font-bold text-2xl">
+                    Eu te amo hoje e pra sempre!
+                    </span>
+                </p>
             </div>
 
         </div>
