01a0da0ff37dc6a3816de7990483d4a12baecbcc
Feijaoozin <gonardo1234@gmail.com>
2024-10-25 22:01:53 -0300

    feat: add pages

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -20,21 +20,45 @@ export default function Header() {
         </Badge>
       </SheetTrigger>
 
-      <SheetContent>
-        <SheetHeader>
+      <SheetContent className="w-52">
+        <SheetHeader className="mb-8">
           <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
 
-        <SheetClose className="flex flex-col gap-5 items-end justify-center w-full bg-red-600">
+        <SheetClose className="flex flex-col gap-5 items-center justify-center w-full">
           <Link href="/">
             <Badge className="h-12 w-32 items-center flex justify-center">
-                Início
+                INÍCIO
             </Badge>
           </Link>
 
           <Link href="./pages/teste">
             <Badge className="h-12 w-32 items-center flex justify-center">
-                Sobre
+                CARTAS & POEMAS
+            </Badge>
+          </Link>
+
+          <Link href="./pages/teste">
+            <Badge className="h-12 w-32 items-center flex justify-center">
+                FOTOS
+            </Badge>
+          </Link>
+
+          <Link href="./pages/teste">
+            <Badge className="h-12 w-32 items-center flex justify-center">
+                FILMES FAVORITOS
+            </Badge>
+          </Link>
+
+          <Link href="./pages/teste">
+            <Badge className="h-12 w-32 items-center flex justify-center">
+                JOGOS
+            </Badge>
+          </Link>
+
+          <Link href="./pages/teste">
+            <Badge className="h-12 w-32 items-center flex justify-center">
+                SOBRE
             </Badge>
           </Link>
         </SheetClose>
